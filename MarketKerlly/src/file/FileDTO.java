4c2bff4d22f4ab7bf691bea55d71c04dde50598e
inho <leeinhoo258@gmail.com>
2020-06-02 15:12:01 +0900

    메인페이지 수정완료

---
+++ MarketKerlly/src/file/FileDTO.java
@@ -0,0 +1,31 @@
+package file;
+
+public class FileDTO {
+	private int price;
+	private String img;
+	private String pro_name;
+
+	public String getPro_name() {
+		return pro_name;
+	}
+
+	public void setPro_name(String pro_name) {
+		this.pro_name = pro_name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+}

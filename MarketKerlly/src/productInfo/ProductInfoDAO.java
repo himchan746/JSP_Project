9eb1d4277431e9735df0c1d7cd2ff6937895ddad
inho <leeinhoo258@gmail.com>
2020-06-02 12:58:37 +0900

    0602 - 12시 58분 최신

--- MarketKerlly/src/productInfo/ProductInfoDAO.java
+++ MarketKerlly/src/productInfo/ProductInfoDAO.java
@@ -8,8 +8,8 @@ public class ProductInfoDAO {
 	private Connection con;
 	public ProductInfoDAO() {
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "ajh","1234");
+			Class.forName("oracle.jdbc.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "jsp","1234");
 		} catch (ClassNotFoundException e) {
 			System.out.println("driver not found");
 		} catch (SQLException e) {
